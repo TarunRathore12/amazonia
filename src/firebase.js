6ae3009c690a1090b59e5e17a238fe357bbaf60d
TarunRathore12 <rathore24tarun@gmail.com>
2021-12-02 17:27:12 +0530

    origin

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCNKQv6lLcqo1x4P7XdKbo1rFtqpQHCDiM",
+    authDomain: "ia-31b13.firebaseapp.com",
+    projectId: "ia-31b13",
+    storageBucket: "ia-31b13.appspot.com",
+    messagingSenderId: "477251643486",
+    appId: "1:477251643486:web:8d3f611ae3c491ecf2eccf",
+    measurementId: "G-R9TZZ7VFY7"
+  };
